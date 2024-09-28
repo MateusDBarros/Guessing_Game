3b9879a807729886ae22ca52fd3a55cb30be3dc9
MateusDBarros <97922225+MateusDBarros@users.noreply.github.com>
2024-09-28 01:23:39 -0300

    Update Guessing.c

--- Guess/Guessing.c
+++ Guess/Guessing.c
@@ -2,40 +2,40 @@
 #include <stdlib.h>
 #include <time.h>
 
-int check(int input, int random, int tries);
+int check(int input, int random, int tentativas);
 int main(void)
 {
     srand(time(0));
 
-    int random_number = (rand() % 20) + 1;
-    int guess;
-    int score = 0;
+    int numero_aleatorio = (rand() % 20) + 1;
+    int palpite;
+    int tentativas = 0;
 
-    check(guess, random_number, score);
+    check(palpite, numero_aleatorio, tentativas);
 }
 
-int check(int input, int random, int tries)
+int check(int input, int random, int tentativas)
 { 
-    printf("Guess a number between 0 - 20: ");
+    printf("Adivinhe um numero entre 0 - 20: ");
     scanf("%d", &input);
-    tries++;
+    tentativas++;
 
     if (input  < 0 || input > 20)
     {
-        printf("Number out of range!\n");
-        return check(input, random, tries);
+        printf("Numero invalido, encolha apenas entre 0 - 20!\n");
+        return check(input, random, tentativas);
     }
 
     if (random == input)
     {
-        printf("You got it right, Congrats the number was %d\n", random);
-        printf("Number of tries: %d\n", tries);
+        printf("Parabens voce acertou, o numero era: %d\n", random);
+        printf("Numero de tentativas: %d\n", tentativas);
         return 0;
     }
 
     else if (random != input)
     {
-        printf("Sorry, wrong number, try again\n");
-        return check(input, random, tries);
+        printf("Desculpe numero errado, Tente novamente\n");
+        return check(input, random, tentativas);
     }
 }
